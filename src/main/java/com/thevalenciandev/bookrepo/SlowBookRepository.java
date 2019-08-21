cff61783d51e30e224353feae1ccb997ba0a781b
thevalenciandev <thevalenciandev@gmail.com>
2019-08-21 18:23:43 +0100

    Enabling caching (ConcurrentHashMap by default)

--- src/main/java/com/thevalenciandev/bookrepo/SlowBookRepository.java
+++ src/main/java/com/thevalenciandev/bookrepo/SlowBookRepository.java
@@ -1,11 +1,13 @@
 package com.thevalenciandev.bookrepo;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SlowBookRepository implements BookRepository {
 
     @Override
+    @Cacheable("books")
     public Book getByIsbn(String isbn) {
         simulateSlowness();
         return new Book(isbn, "Some book");
